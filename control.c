db553e4fcf620e0c535e65ca4f1d708f4f66d6cf
pqhwan <hyun_sik_kim@brown.edu>
2015-02-03 12:30:50 -0500

    gettin started

---
+++ control.c
@@ -0,0 +1,64 @@
+control for Saja server
+
+top:
+    * design API endpoints
+        - do we need websockets for this? probably for notifications?:
+    * beginning
+        * learn jasmine for simple unit testing (don't try anything crazy)
+        * write a start script
+        * get package.json right
+        * actually write README.md
+        * tools
+            * vim-node
+    - recruiting
+
+
+
+
+
+learning:
+    * node
+        * HTTP
+             http server events
+            - http server EVENTS: request, connection, close, connect, upgrade
+        * HTTPS
+        * Events
+        * File System
+        * Modules
+        * TLS/SSL
+        * Utilities
+        * Debugger
+        * Path
+        * Assertion Testing
+        * Stream
+        * Query Strings
+        * URL
+        * Readline
+
+        * Buffer
+        * C/C++ Addons
+        * Child Processes
+        * Cluster
+        * Console
+        * Crypto
+        * DNS
+        * Domain
+        * Globals
+        * Net
+        * OS
+        * Process
+        * Punycode
+        * REPL
+        * String Decoder
+        * Timers
+        * TTY
+        * UDP/Datagram
+        * VM
+        * ZLIB
+    * express
+        * application
+        * request
+        * response
+        * router
+        
+    * mongoose
